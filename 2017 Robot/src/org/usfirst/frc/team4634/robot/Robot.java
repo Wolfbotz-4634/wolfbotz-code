91ee085902c17ceb074dfee5ff88d2f19348406c
oscarwong67 <oscarwong67@gmail.com>
2017-04-06 10:25:10 -0600

    updated stuff don't ask

--- 2017 Robot/src/org/usfirst/frc/team4634/robot/Robot.java
+++ 2017 Robot/src/org/usfirst/frc/team4634/robot/Robot.java
@@ -1,4 +1,5 @@
 package org.usfirst.frc.team4634.robot;
+
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.command.Command;
@@ -20,7 +21,7 @@ import edu.wpi.first.wpilibj.vision.VisionThread;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.Joystick;
-
+import edu.wpi.first.wpilibj.DigitalInput;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -32,193 +33,221 @@ import edu.wpi.first.wpilibj.Joystick;
 public class Robot extends IterativeRobot {
 	public static OI oi;
 	public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
-	
-	//vars for vision processing
+
+	// vars for vision processing
 	private static final int IMG_WIDTH = 320;
-	private static final int IMG_HEIGHT = 240;	
+	private static final int IMG_HEIGHT = 240;
 	private VisionThread visionThread;
-	private double centerX = 0.0;	
+	private double centerX = 0.0;
 	private final Object imgLock = new Object();
-	Boolean gearPlaced;	
-    Command autonomousCommand;
-    RobotDrive myRobot; //the robot's driving functionality
-    Timer timer; //a timer that counts in seconds
-    Joystick driveStick, mechanismStick;
-    //XboxController driveXbox, mechanismXbox; //driver's controller, mechanism control's controller
-    
-  
-    Solenoid rangeFinderPower;
+	Boolean gearPlaced;
+	Command autonomousCommand;
+	RobotDrive myRobot; // the robot's driving functionality
+	Timer timer; // a timer that counts in seconds
+	Joystick driveStick, mechanismStick;
+	DigitalInput limitSwitch;
 
-    VictorSP leftRear, leftFront, rightRear, rightFront;
-    Talon middleMotor, shootingMotor, climbingMotor; //middle strafing motor
-    boolean brakeYes;
-    SendableChooser chooser;
-    
-    @SuppressWarnings("rawtypes")
-    
+	// XboxController driveXbox, mechanismXbox; //driver's controller, mechanism
+	// control's controller
+
+	Solenoid rangeFinderPower;
+
+	VictorSP leftRear, leftFront, rightRear, rightFront;
+	Talon middleMotor, shootingMotor, climbingMotor; // middle strafing motor
+	boolean brakeYes;
+	SendableChooser chooser;
+
+	@SuppressWarnings("rawtypes")
+
 	/**
-     * This function is run when the robot is first started up and should be
-     * used for any initialization code.
-     */
-    @Override
-    public void robotInit() {
-        chooser = new SendableChooser();
-    	oi = new OI();
-    	chooser.addDefault("Default Auto", new ExampleCommand());
-    	
-    	/*
-        sensor = new AnalogInput(0);
-        rangefinder = new RangeFinding(sensor);      
-        rangeFinderPower = new Solenoid(0);
-        rangeFinderPower.set(true); */ //ONLY INITIALIZE IF WE'RE USING AN ANALOG RANGEFINDER
-    	
-    	myRobot = new RobotDrive(0,1,2,3);
-    	timer = new Timer();        
-        gearPlaced = true;
-        driveStick = new Joystick(0);
-        mechanismStick = new Joystick(1);
-        
-        /*driveXbox = new XboxController(0);
-        mechanismXbox = new XboxController(1);*/
-        middleMotor = new Talon(4);
-        //shootingMotor = new Talon(5);
-        climbingMotor = new Talon(5);
-        SmartDashboard.putData("Auto mode", chooser);
-        UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
-        camera.setResolution(IMG_WIDTH, IMG_HEIGHT);
-        
-        visionThread = new VisionThread(camera, new GripPipeline(), pipeline -> {
-        	visionThread.setDaemon(true);
-        	while (!Thread.interrupted()) {
-        		if (!pipeline.filterContoursOutput().isEmpty()) {
-                    Rect r = Imgproc.boundingRect(pipeline.filterContoursOutput().get(0));
-                    synchronized (imgLock) {
-                        centerX = r.x + (r.width / 2);
-                    }
-                }
-        	}
-        });
-        visionThread.start();
-    }
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
+	 */
+	@Override
+	public void robotInit() {
+		chooser = new SendableChooser();
+		oi = new OI();
+		chooser.addDefault("Default Auto", new ExampleCommand());
 
-    public void autonomousInit() { //currently configured for far right side start
-        autonomousCommand = (Command) chooser.getSelected();
-        if (autonomousCommand != null) autonomousCommand.start();        
-        
-        //FAR RIGHT START
-        driveForTime(1.0, 1.0);
-        Timer turningTimer = new Timer();
-        turningTimer.reset();
-        turningTimer.start();
-        while (turningTimer.get() < 1.5) {
-        	myRobot.arcadeDrive(0.0, -1.0);
-        }
-        driveForTime(0.1, 1.0);
+		/*
+		 * sensor = new AnalogInput(0); rangefinder = new RangeFinding(sensor);
+		 * rangeFinderPower = new Solenoid(0); rangeFinderPower.set(true);
+		 */ // ONLY INITIALIZE IF WE'RE USING AN ANALOG RANGEFINDER
 
-        //MIDDLE START
-        //driveForTime(0.5, 1.0);    
-    }
+		myRobot = new RobotDrive(0, 1, 2, 3);
+		timer = new Timer();
+		gearPlaced = true;
+		driveStick = new Joystick(0);
+		mechanismStick = new Joystick(1);
 
-    public void autonomousPeriodic() { 
-        Scheduler.getInstance().run();
-        /*this gets the centerX pixel value of the target and subtracts half the image width to change
-         *  it to a value that is zero when the rectangle is centered in the image and positive or negative when
-         *   the target center is on the left or right side of the frame. That value is used to steer the robot 
-         *   towards the target.*/
-        
-        //FAR RIGHT START
-        if (! gearPlaced) {
-        	double centerX;
-        	synchronized (imgLock) {
-        		centerX = this.centerX;
-        	}
-        	double turn = centerX - (IMG_WIDTH / 2);
-        	if (turn > 0) {
-                myRobot.arcadeDrive(-0.6, turn * 0.005);
-            } /*else {
-                while (rangefinder.getRange() > 6.0) {
-                    myRobot.drive(0.75, 0.0);
-                    myRobot.drive(0.0, 0.0);
-                    gearPlaced = true;
-                    driveForTime(1.0, -0.75);
-                }   //USE ONLY IF WE HAVE A RANGEFINDER
-            }*/ else {
-                myRobot.drive(0.5, 0.0);
-            }
-        } else {
-            //What do we do once the gear has been placed
-        }
+		/*
+		 * driveXbox = new XboxController(0); mechanismXbox = new
+		 * XboxController(1);
+		 */
+		middleMotor = new Talon(4);
+		// shootingMotor = new Talon(5);
+		climbingMotor = new Talon(5);
+		SmartDashboard.putData("Auto mode", chooser);
+		UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
+		camera.setResolution(IMG_WIDTH, IMG_HEIGHT);
 
-        //MIDDLE START
+		visionThread = new VisionThread(camera, new GripPipeline(), pipeline -> {
+			visionThread.setDaemon(true);
+			while (!Thread.interrupted()) {
+				if (!pipeline.filterContoursOutput().isEmpty()) {
+					Rect r = Imgproc.boundingRect(pipeline.filterContoursOutput().get(0));
+					synchronized (imgLock) {
+						centerX = r.x + (r.width / 2);
+					}
+				}
+			}
+		});
+		visionThread.start();
+	}
 
-        //if we don't have one:
-        //TBD
-        /*
-        if (! gearPlaced) {
-        	myRobot.drive(0.5, 0.0;
-        }
-    	*/
-        
-        
-    }
+	public void autonomousInit() { // currently configured for far right side
+									// start
+		autonomousCommand = (Command) chooser.getSelected();
+		if (autonomousCommand != null)
+			autonomousCommand.start();
 
-    public void teleopInit() {    	
-        if (autonomousCommand != null) autonomousCommand.cancel(); //don't touch
-    }
+		// FAR RIGHT START
+		driveForTime(0.9, 1.0);
+		Timer turningTimer = new Timer();
+		turningTimer.reset();
+		turningTimer.start();
+		while (turningTimer.get() < 0.25) {
+			myRobot.arcadeDrive(0.0, -0.5);
+		}
+		driveForTime(0.1, 1.0);
 
-    public void teleopPeriodic() {
-        Scheduler.getInstance().run();       
-        myRobot.arcadeDrive(driveStick.getY(), -driveStick.getX());
-        middleMotor();
-        if (mechanismStick.getRawButton(2)) {
-        	climbingMotor.set(1.0);
-        }
-        /*if (mechanismStick.getRawButton(1)) {
-        	shootingMotor.set(1.0);
-        }*/
-    }
-    
-    public void middleMotor() {
-    	while (driveStick.getRawButton(8)) {
-    		middleMotor.set(1.0);
-    	}
-    	while (driveStick.getRawButton(9)) {
-    		middleMotor.set(-1.0);
-    	}
-    	while (mechanismStick.getRawButton(8)) {
-    		middleMotor.set(1.0);
-    	}
-    	while (mechanismStick.getRawButton(9)) {
-    		middleMotor.set(-1.0);
-    	}
-    	middleMotor.set(0);
-    }
+		// MIDDLE START
+		// driveForTime(0.75, 1.0);
+	}
 
-    //drives forward for a specified amount of time
-    public void driveForTime(double time, double speed) {
-    	timer.reset();
-        timer.start();
-    	while (timer.get() < time) {
-    		myRobot.drive(speed, 0.0);
-    	}
-    }    
-    
-    //drives in reverse for a specified amount of time
-    public void reverse(double time) {
-    	timer.reset();
-        timer.start();
-    	while (timer.get() < time) {
-    		myRobot.drive(-0.40, 0.0);
-    	}
-    }
-      
-    public void disabledInit(){    	  
-    }	
-  	public void disabledPeriodic() {
-  		Scheduler.getInstance().run();
-  	}
+	public void autonomousPeriodic() {
+		Scheduler.getInstance().run();
+		/*
+		 * this gets the centerX pixel value of the target and subtracts half
+		 * the image width to change it to a value that is zero when the
+		 * rectangle is centered in the image and positive or negative when the
+		 * target center is on the left or right side of the frame. That value
+		 * is used to steer the robot towards the target.
+		 */
 
-    public void testPeriodic() {
-        LiveWindow.run();
-    }
+		// FAR RIGHT START
+		if (limitSwitch.get()) {
+			double centerX;
+			synchronized (imgLock) {
+				centerX = this.centerX;
+			}
+			double turn = centerX - (IMG_WIDTH / 2);
+			if (turn > 0) {
+				myRobot.arcadeDrive(-0.6, turn * 0.005);
+			} /*
+				 * else { while (rangefinder.getRange() > 6.0) {
+				 * myRobot.drive(0.75, 0.0); myRobot.drive(0.0, 0.0); gearPlaced
+				 * = true; driveForTime(1.0, -0.75); } //USE ONLY IF WE HAVE A
+				 * RANGEFINDER }
+				 */ else {
+				myRobot.drive(0.5, 0.0);
+			}
+		} else {
+		// What do we do once the gear has been placed
+		Timer turningTimer = new Timer();
+		reverse(0.5);		
+		turningTimer.reset();
+		turningTimer.start();
+		while (turningTimer.get() < 0.25) {
+			myRobot.arcadeDrive(0.0, 0.5);
+		}
+		driveForTime(3.0, 1.0);
+		}
+		// MIDDLE START
+
+		// if we don't have one:
+		// TBD
+
+		/*if (limitSwitch.get()) {
+			myRobot.drive(0.5, 0.0);
+		} else {
+		// What do we do once the gear has been placed
+		reverse(0.5);
+		turningTimer.reset();
+		turningTimer.start();
+		while (turningTimer.get() < 0.5) {
+			myRobot.arcadeDrive(0.0, 0.5);
+		}
+		driveForTime(1.0, 1.0);
+		turningTimer.reset();
+		turningTimer.start();
+		while (turningTimer.get() < 0.5) {
+			myRobot.arcadeDrive(0.0, -0.5);
+		}
+		driveForTime(3.0, 1.0);
+		}
+		*/		
+	}
+
+	public void teleopInit() {
+		if (autonomousCommand != null)
+			autonomousCommand.cancel(); // don't touch
+	}
+
+	public void teleopPeriodic() {
+		Scheduler.getInstance().run();
+		myRobot.arcadeDrive(driveStick.getY(), -driveStick.getX());
+		middleMotor();
+		if (mechanismStick.getRawButton(2)) {
+			climbingMotor.set(1.0);
+		}
+		/*
+		 * if (mechanismStick.getRawButton(1)) { shootingMotor.set(1.0); }
+		 */
+	}
+
+	public void middleMotor() {
+		while (driveStick.getRawButton(8)) {
+			middleMotor.set(1.0);
+		}
+		while (driveStick.getRawButton(9)) {
+			middleMotor.set(-1.0);
+		}
+		while (mechanismStick.getRawButton(8)) {
+			middleMotor.set(1.0);
+		}
+		while (mechanismStick.getRawButton(9)) {
+			middleMotor.set(-1.0);
+		}
+		middleMotor.set(0);
+	}
+
+	// drives forward for a specified amount of time
+	public void driveForTime(double time, double speed) {
+		timer.reset();
+		timer.start();
+		while (timer.get() < time) {
+			myRobot.drive(speed, 0.0);
+		}
+	}
+
+	// drives in reverse for a specified amount of time
+	public void reverse(double time) {
+		timer.reset();
+		timer.start();
+		while (timer.get() < time) {
+			myRobot.drive(-0.40, 0.0);
+		}
+	}
+
+	public void disabledInit() {
+	}
+
+	public void disabledPeriodic() {
+		Scheduler.getInstance().run();
+	}
+
+	public void testPeriodic() {
+		LiveWindow.run();
+	}
 }
